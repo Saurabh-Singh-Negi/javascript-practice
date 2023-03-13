0333e553e5a287d3696083c86e6e29c162e2f58a
Saurabh <saurabhnegi0007@gmail.com>
2023-03-14 00:07:24 +0530

    updated logic for am pm

--- digital-clock/app.js
+++ digital-clock/app.js
@@ -1,6 +1,4 @@
-// const hour = document.getElementById("hours");
-// const min = document.getElementById("min");
-// const sec = document.getElementById("sec");
+
 const timer = document.getElementById("time");
 const ampm_val = document.getElementById("ampm")
 setInterval(() => {
@@ -9,11 +7,10 @@ setInterval(() => {
     let minutes = time.getMinutes();
     let seconds = time.getSeconds();
     let ampm = 'AM';
-    if(hours > 12) {
+    if(hours >= 12) {
         hours = hours%12;
         ampm = 'PM';
     }
-    console.log(ampm)
     
     if(hours < 10) hours = '0' + hours
     if(minutes < 10) minutes = '0' + minutes
@@ -21,8 +18,5 @@ setInterval(() => {
 
     timer.innerText = `${hours}:${minutes}:${seconds}`
     ampm_val.innerText = ampm;
-    // hour.innerText = hours;
-    // min.innerText = minutes;
-    // sec.innerText = seconds;
 },1000)
 
